b75da8613414db7893773eec1b0450930899491d
BuildTools <unconfigured@null.spigotmc.org>
2022-03-10 19:13:17 +0200

    Created backend start

---
+++ BackendLicenta/Context/GeneralContext.cs
@@ -0,0 +1,13 @@
+﻿using BackendLicenta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendLicenta.Context
+{
+    public class GeneralContext : DbContext
+    {
+        public DbSet<Clinica> Clinica { get; set; }
+
+        public GeneralContext(DbContextOptions<GeneralContext> options)
+            : base(options) { }
+    }
+}
---
+++ BackendLicenta/Controllers/ClinicaController.cs
@@ -0,0 +1,27 @@
+﻿using BackendLicenta.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BackendLicenta.Controllers
+{
+    [ApiController]
+    [Route("clinica")]
+    public class ClinicaController : ControllerBase
+    {
+        private readonly GeneralContext context;
+
+        public ClinicaController(GeneralContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClinica()
+        {
+            var clinics = await context.Clinica.ToListAsync();
+
+            return Ok(clinics);
+        }
+    }
+}
